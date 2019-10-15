0b8bf8afd604d19a22f0d8dc3699ede1f5094297
disgraceful <noolic97@gmail.com>
2019-10-15 14:44:15 +0300

    Iterate code decode implement

--- src/codelogic/nonbinary/iteratecode.js
+++ src/codelogic/nonbinary/iteratecode.js
@@ -7,33 +7,57 @@ export const iterateCode = {
     },
     methods: {
         code(message, matrix, q) {
-            let equation = this.makeMatrix(message, matrix);
+            let equation = this.makeMatrix(message, matrix, true);
             for (let i = 0; i < matrix; i++) {
                 equation[i][matrix] = q - (equation[i].reduce(this.reducer) % q);
                 equation[matrix][i] = q - (equation.map(x => x[i]).reduce(this.reducer) % q)
             }
-            console.log(equation);
             let row = q - (equation[matrix].reduce(this.reducer) % q);
             let col = q - (equation.map(x => x[matrix]).reduce(this.reducer) % q)
-            if(col===row){
+            if (col === row) {
                 equation[matrix][matrix] = row;
             }
             return equation;
         },
-        decode(message) {
+        decode(message, matrix, q) {
+            let equation = this.makeMatrix(message, matrix, false);
+            equation = this.fillMatrix(equation);
+
+            for (let i = 0; i < equation.length; i++) {
+                equation[i][equation.length - 1] = equation[i].reduce(this.reducer) % q;
+                equation[equation.length - 1][i] = equation.map(x => x[i]).reduce(this.reducer) % q;
+            }
+            console.log(equation);
+            return this.correct(equation);
 
         },
-        makeMatrix(message, matrix) {
+        makeMatrix(message, matrix, coding) {
             let equation = [];
             for (let i = 1; i <= matrix + 1; i++) {
                 let row = [];
                 for (let j = 1; j <= matrix + 1; j++) {
-                    if (i > matrix || j > matrix) row.push(0);
+                    if ((i > matrix || j > matrix) && coding) {
+                        row.push(0);
+                    }
                     else row.push(parseInt(message[`${i}${j}`]));
                 }
                 equation.push(row);
             }
             return equation
+        }, fillMatrix(oldMatrix) {
+            let newRow = [];
+            for (let i = 0; i < oldMatrix.length; i++) {
+                oldMatrix[i].push(0);
+                newRow.push(0);
+            }
+            newRow.push(0);
+            oldMatrix.push(newRow);
+            console.log(oldMatrix);
+            return oldMatrix;
+        },
+        correct(matrix) {
+            return matrix[matrix.length - 1].reduce(this.reducer) === 0 &&
+                matrix.map(x => x[matrix.length - 1]).reduce(this.reducer) === 0;
         }
     }
 }
--- src/components/codes/iterateCode/IterateDecode.vue
+++ src/components/codes/iterateCode/IterateDecode.vue
@@ -1,5 +1,126 @@
 <template>
-    <div>
-        DeCode
-    </div>
+  <v-card flat>
+    <v-container>
+      <v-slider
+        v-model="slider.value"
+        :max="slider.max"
+        :min="slider.min"
+        label="Matrix"
+        @change="generate()"
+      >
+        <template v-slot:append>
+          <v-text-field
+            v-model="slider.value"
+            :min="slider.min"
+            :max="slider.max"
+            class="mt-0 pt-0"
+            hide-details
+            single-line
+            type="number"
+            style="width: 50px"
+            @input="generate()"
+          ></v-text-field>
+        </template>
+      </v-slider>
+    </v-container>
+    <v-card-actions>
+      <v-col>
+        <v-row v-for="i in slider.value+1" :key="i">
+          <v-col style="max-width: 70px" v-for="j in slider.value+1" :key="j">
+            <v-text-field style="width: 50px" v-model.number="values[`${i}${j}`]"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-card-actions>
+    <v-text-field label="Q" style="width: 50px" v-model="q"></v-text-field>
+    <v-text-field label="Mistakes" style="width: 50px" v-model="mistakes"></v-text-field>
+    <v-row>
+      <v-col>
+        <v-btn @click="generateWithMistake()">Generate</v-btn>
+        <v-btn @click="test()">TestDecode</v-btn>
+        <v-btn :disabled="!check()" @click="submit()">Sumbit</v-btn>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
+
+<script>
+import { iterateCode } from "../../../codelogic/nonbinary/iteratecode";
+export default {
+  data() {
+    return {
+      q: 3,
+      mistakes: 1,
+      values: {},
+      slider: {
+        min: 2,
+        max: 6,
+        value: 2
+      }
+    };
+  },
+  mixins: [iterateCode],
+  methods: {
+    test() {
+      console.log(this.decode(this.values, this.slider.value, this.q));
+    },
+    check() {
+      let keys = Object.keys(this.values);
+      let result = keys.every(
+        x => this.values[x] !== "" && this.values[x] !== undefined
+      );
+      return result;
+    },
+    generateWithMistake() {
+      this.generate();
+      let coded = this.code(this.values, this.slider.value, this.q);
+      console.log(coded);
+
+      for (let i = 1; i <= coded.length; i++) {
+        for (let j = 1; j <= coded.length; j++) {
+          this.values[`${i}${j}`] = coded[i - 1][j - 1];
+        }
+      }
+      console.log(this.values);
+      let counter = this.mistakes;
+      while (counter > 0) {
+       this.createMistake();
+        counter--;
+      }
+
+      this.q += 1;
+      this.q -= 1;
+    },
+    generate() {
+      for (const key in this.values) {
+        this.values[key] = "";
+      }
+      for (let i = 1; i <= this.slider.value; i++) {
+        for (let j = 1; j <= this.slider.value; j++) {
+          this.values[`${i}${j}`] = Math.round(Math.random() * 9);
+        }
+      }
+      this.q = Math.round(Math.random() * 9);
+    },
+    submit() {
+      let answer = this.code(this.values, this.slider.value, this.q);
+    },
+    createMistake() {
+      let randRow = Math.round(Math.random() * this.slider.value);
+      let randCol = Math.round(Math.random() * this.slider.value);
+      console.log(randRow, randCol);
+      let oldValue = this.values[`${randRow}${randCol}`];
+      let newValue = (oldValue - Math.round(Math.random() * 9)) % this.q;
+      newValue =
+        newValue === oldValue || newValue - oldValue === this.q
+          ? oldValue -1 
+          : newValue;
+      this.values[`${randRow}${randCol}`] = this.values[`${randRow}${randCol}`];
+    }
+  },
+
+  created() {
+    //this.generate();
+  }
+};
+</script>

bf6022bea1bdaf7f4cf0150b931aa1c6742454f0
disgraceful <noolic97@gmail.com>
2019-10-25 10:47:24 +0300

    shennon fano algorithm

--- src/codelogic/othercodes/shennonfano.js
+++ src/codelogic/othercodes/shennonfano.js
@@ -1,3 +1,65 @@
+/* eslint-disable */
 export const shennonFano = {
-    
+    data() {
+        return {
+            reducer: (accumulator, curValue) => accumulator + curValue,
+            first: [],
+            second: [],
+            temp: [],
+            answers: null
+
+
+        }
+    },
+    methods: {
+        code(values) {
+            this.answers = new Map();
+            values = values.map(e => Math.round(e * 100));
+            this.cycle(values);
+
+            console.log(this.answers);
+        },
+        cycle(array) {
+            for (let i = 0; i < array.length; i++) {
+                this.first = array.slice(0, i + 1);
+                console.log("first", this.first);
+                this.second = array.slice(i + 1);
+                console.log("second", this.second);
+                let firstSum = this.sumUp(this.first);
+                let secondSum = this.sumUp(this.second);
+                if (Math.abs(secondSum - firstSum) < array[i + 1] || array.length <= 2) {
+                    this.mapValues(this.first, "0");
+                    this.mapValues(this.second, "1");
+                    break;
+                }
+            }
+            if(!this.getLastMapValue().includes("0") && this.getLastMapValue().length>1) {this.temp = []}
+            if (this.first.length > 1 && array.length > 2) {
+                this.temp = this.second;
+                this.cycle(this.first);
+            }
+            if (this.second.length > 1 && array.length > 2) {
+                this.temp = this.second;
+                this.cycle(this.second);
+            }
+            if (this.temp.length > 1 && array.length > 2) { this.cycle(this.temp); }
+        },
+        mapValues(array, value) {
+            array.forEach(e => {
+                let curValue = this.answers.get(e);
+                if (curValue === undefined) {
+                    this.answers.set(e, value);
+                } else {
+                    curValue += value;
+                    this.answers.set(e, curValue);
+                }
+            });
+        },
+        getLastMapValue() {
+            return Array.from(this.answers.values()).pop();
+        },
+        sumUp(array) {
+            return array.reduce(this.reducer);
+        }
+    }
 }
--- src/components/codes/shennonFanoCode/ShennonFanoCode.vue
+++ src/components/codes/shennonFanoCode/ShennonFanoCode.vue
@@ -28,9 +28,11 @@
             <tbody>
               <tr v-for="n in slider.value" :key="n">
                 <td>P{{n-1}}</td>
-                <td>Random Value</td>
                 <td>
-                  <v-text-field v-model="values[`${n}`]"></v-text-field>
+                  <v-text-field readonly v-model="randoms[n-1]"></v-text-field>
+                </td>
+                <td>
+                  <v-text-field v-model="answers[`${n}`]"></v-text-field>
                 </td>
               </tr>
             </tbody>
@@ -38,7 +40,8 @@
         </v-simple-table>
       </v-col>
       <v-col>
-        <v-btn @click="test()">Test</v-btn>
+        <v-btn @click="generate()">Generate</v-btn>
+        <v-btn @click="sequence()">Code</v-btn>
       </v-col>
       <v-dialog v-model="dialog" max-width="330px" hide-overlay>
         <code-dialog
@@ -65,7 +68,7 @@ export default {
         value: 6
       },
       correct: false,
-      values: {},
+      answers: {},
       randoms: []
     };
   },
@@ -82,24 +85,48 @@ export default {
       }
     },
     test() {
-      console.log(this.values);
+      this.generate();
     },
-    generate() {},
+    check() {},
+    sequence() {
+      let answer = this.code(this.randoms);
+    },
+    generate() {
+      this.randoms = [];
+      let total = 100;
+      let remainder = this.slider.value - 6;
+      remainder = remainder > 0 ? 5 - remainder : 5;
+      let max = remainder * this.slider.value;
+      let min = max - remainder;
+      for (let i = 0; i < this.slider.value; i++) {
+        let minus = this.random(min, max);
+        this.randoms.push(minus);
+        console.log(`${i}: ${minus}`);
+        total -= minus;
+        max -= remainder;
+        min = max - remainder + 1;
+      }
+      console.log(total);
+      while (total != 0) {
+        for (let i = 0; i < this.randoms.length; i++) {
+          if (total > 0 && total !== 0) {
+            this.randoms[i]++;
+            total--;
+          } else if (total < 0 && this.randoms[i] > 1 && total !== 0) {
+            this.randoms[i]--;
+            total++;
+          }
+        }
+      }
+      this.randoms = this.randoms.map(e => e / 100);
+      console.log(`${total}`, this.randoms);
+    },
     random(min, max) {
-      return Math.round(Math.rand() * (max - min) + min);
+      return Math.round(Math.random() * (max - min) + min);
     }
   },
-  created() {
-    this.randoms = [];
-    let total = 100;
-    let min = 1;
-    let max = 5;
-    let sum = 0;
-    for (let i = 0; i < this.slider.value; i++) {
-      let minus = this.random(min, max);
-      this.randoms.push(minus);
-      sum+=minus;
-    }
+  created(){
+    this.generate();
   }
 };
 </script>
